35664c16cd4319dac285eec7cacbd0bd362ffc2f
raoulkabore39-sketch <raoulkabore39@gmail.com>
2026-07-21 01:04:30 +0000

    Delete src/src/app.tsx

--- src/src/app.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '20px', fontfamily:
-'sans-serif' }}>
-      <h1> Mon Template est en ligne !</h1>
-      <p> Le site fonctionne parfaitement.</p>
-    </div>
-    )
-}
