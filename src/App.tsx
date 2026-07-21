9f892e1a8826f79c84f10984273227be094e6805
raoulkabore39-sketch <raoulkabore39@gmail.com>
2026-07-21 01:01:22 +0000

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontfamily:
+'sans-serif' }}>
+      <h1> Mon Template est en ligne !</h1>
+      <p> Le site fonctionne parfaitement.</p>
+    </div>
+    )
+}
